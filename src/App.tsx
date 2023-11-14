1eeae539bedeea0534bbf1c644f159673dc53ac3
huytv04883 <huytv04883@gmail.com>
2023-11-15 02:51:55 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import TodoListApp from "./components/TodoList/TodoList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Todos</h1>
+      <TodoListApp />
+    </div>
+  );
+}
+
+export default App;
